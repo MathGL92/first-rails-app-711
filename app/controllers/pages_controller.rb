a0699c7bb30eeea2c064e6ad4ee39022db647c30
Mathieu Longe <mathieu.longe@orange.fr>
2021-11-09 20:38:17 +1100

    Add home, about and contact pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+
+  def about
+  end
+
+  def home
+  end
+
+  def contact
+    @members = [ 'thanh', 'dimitri', 'germain', 'damien', 'julien' ]
+
+    if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
+  end
+end
